2e865394e2b93100dca70860637f41d0ed1ee2ff
mixa000012 <shirokoffson@mail.ru>
2023-10-15 09:21:56 +0300

    add ssl

--- docker/app.sh
+++ docker/app.sh
@@ -4,4 +4,4 @@ alembic upgrade head
 
 python fill.py
 
-uvicorn app.main:app --host=0.0.0.0 --port 8000
+uvicorn app.main:app --host=0.0.0.0 --port 8000 --ssl-keyfile="privkey.pem" --ssl-certfile="fullchain.pem"
